34ea9754da08fa7fe7942c2e61f359bd759573b5
JirapatK7 <69011907@kmitl.ac.th>
2026-07-10 08:59:10 +0000

    lab1

--- week1-2/ex001.c
+++ week1-2/ex001.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 int main(){
-    printf("69011907 Jirapat Kusujarit\n");
+    printf("69011907 Jirapat Kusujarit\n\n");
+    printf("%s\n","Jirapat K.");
+    printf("%d\n",100);
+    printf("%f\n", 4.55);
+    printf("%c\n",'A');
+
+    printf("%s %d %f %c\n","Jirapat K.", 100,4.55,'A');
     return 0;
 }
